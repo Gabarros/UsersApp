b45f658ec76bb1c88f974a509e74a0d4c00cf4e6
Gabarros <gabarros17@gmail.com>
2019-10-03 23:54:46 -0300

    Add express validator

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,9 @@ let db = new NeDb({
 
 module.exports = (app) => {
 
-    app.get('/users', (req, res) => {
+    let route = app.route('/users');
+
+    route.get((req, res) => {
 
         db.find({}).sort({name:1}).exec((err, users)=>{
             if(err){
@@ -18,24 +20,22 @@ module.exports = (app) => {
                 res.setHeader('Content-Type', 'application/json');
                 res.json({
                     users
-                })
+                });
             }
         });
 
-
-        
-        
-
     });
 
-    app.post('/users', (req, res) => {
+    route.post((req, res) => {
 
-        db.insert((req.body), (err, user)=>{
+        if(!app.utils.validator.user(app, req, res)) return false;
 
-            if(err) {
+        db.insert((req.body), (errors, user)=>{
 
-                app.utils.error.send(err, req, res);
-                
+            if(errors) {
+
+                app.utils.error.send(errors, req, res);
+                return false;
             }else{
                 res.status(200).json(user);
 
@@ -43,5 +43,54 @@ module.exports = (app) => {
         });
 
     });
+
+    let routeId = app.route('/users/:id');
+
+    routeId.get((req, res)=>{
+
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
+
+            if(err) {
+
+                app.utils.error.send(err, req, res);
+
+            }else{
+                res.status(200).json(user);
+
+            }
+        });
+    });
+
+    routeId.put((req, res)=>{
+
+        if(!app.utils.validator.user(app, req, res)) return false;
+
+        db.update({_id: req.params.id}, req.body, err => {
+ 
+            if(errors) {
+
+                app.utils.error.send(errors, req, res);
+                return false;
+            }else{
+                res.status(200).json(user);
+
+            }
+        });
+    });
+
+    routeId.delete((req, res)=>{
+
+        db.remove({_id: req.params.id}, {}, err => {
+
+            if(err) {
+
+                app.utils.error.send(err, req, res);
+
+            }else{
+                res.status(200).json(req.params);
+
+            }
+        });
+    })
 
 };
