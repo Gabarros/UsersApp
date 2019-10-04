6b6a447428664ef6468f6fca16c9149820e86dfa
Gabarros <gabarros17@gmail.com>
2019-10-03 22:48:52 -0300

    add body parser

--- routes/users.js
+++ routes/users.js
@@ -13,18 +13,9 @@ module.exports = (app) => {
 
     });
 
-    app.get('/users/admin', (req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: [{
-                name: 'Gabriel',
-                idade: 23,
-                type: "Admin",
-                id: 1
-            }]
-        });
+    app.post('/users', (req, res) => {
 
+        res.json(req.body);
 
     });
 
