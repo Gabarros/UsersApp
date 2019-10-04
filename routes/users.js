d831aa4a11c598dc222a0e7ecdd7ba22080aa0e9
Gabarros <gabarros17@gmail.com>
2019-10-03 22:42:01 -0300

    Add Consign

--- routes/users.js
+++ routes/users.js
@@ -1,35 +1,31 @@
-let express = require('express');
-let routes = express.Router();
+module.exports = (app) => {
+    app.get('/users', (req, res) => {
 
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                name: 'Gabriel',
+                idade: 23,
+                id: 1
+            }]
+        });
 
-
-routes.get('/', (req, res)=>{
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users:[{
-            name: 'Gabriel',
-            idade: 23,
-            id: 1
-        }]
     });
 
-});
+    app.get('/users/admin', (req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({
+            users: [{
+                name: 'Gabriel',
+                idade: 23,
+                type: "Admin",
+                id: 1
+            }]
+        });
 
-routes.get('/admin',(req, res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({
-        users:[{
-            name: 'Gabriel',
-            idade: 23,
-            type: "Admin",
-            id: 1
-        }]
+
     });
 
-
-})
-
-module.exports = routes;
+};
